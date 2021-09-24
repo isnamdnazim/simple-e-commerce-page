e7df9939e24f58332f8d3c0a1aa5c480461a32a7
isnamdnazim <isnamdnazim@gmail.com>
2021-09-24 17:51:44 +0600

    font awesome cdn added

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 import "./Product.css";
+import Rating from 'react-rating';
 
 const Product = (props) => {
     const cartIcon = <FontAwesomeIcon icon={faShoppingCart} />
@@ -17,6 +18,10 @@ const Product = (props) => {
                 <p><small>By: {seller}</small></p>
                 <p>Price: {price}$</p>
                 <p><small>Only {stock} left in stock - order soon</small></p>
+                <Rating
+                    emptySymbol="fa fa-star-o fa-2x"
+                    fullSymbol="fa fa-star fa-2x"
+                    readonly /><br />
                 <button
                     onClick={() => props.handleAddToCart(props.product)}
                     className="purches-button"
