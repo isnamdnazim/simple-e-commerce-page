222fdaa3ab19c47e6841a2a0949f8e65d87c1acb
isnamdnazim <isnamdnazim@gmail.com>
2021-10-01 16:18:04 +0600

    route setup complete for this webpage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Header from './Components/Header/Header';
+import Inventory from './Components/Inventory/Inventory';
+import OrderReview from './Components/OrderReview/OrderReview';
 import Shop from './Components/Shop/Shop';
+import NotFound from './Components/NotFound/NotFound';
 
 function App() {
   return (
     <div>
       <Header />
-      <Shop />
+
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Shop />
+          </Route>
+          <Route path="/shop">
+            <Shop />
+          </Route>
+          <Route path="/inventory">
+            <Inventory />
+          </Route>
+          <Route path="/orderreview">
+            <OrderReview />
+          </Route>
+          <Route path="/order">
+            <OrderReview />
+          </Route>
+          <Route path="*">
+            <NotFound />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+
+
     </div>
   );
 }
---
+++ src/Components/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            <h1>404</h1>
+            <h2>The thing you want to try to found is not here!</h2>
+        </div>
+    );
+};
+
+export default NotFound;
