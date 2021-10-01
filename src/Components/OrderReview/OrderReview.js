d24aa861497424731d08fb984a9243fe78efc07d
isnamdnazim <isnamdnazim@gmail.com>
2021-10-01 16:42:46 +0600

    create a function in hooks folder named useProducts by useing hooks

--- src/Components/OrderReview/OrderReview.js
+++ src/Components/OrderReview/OrderReview.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import useProducts from '../../Hooks/useProducts';
 
 const OrderReview = () => {
+    const [products] = useProducts();
     return (
         <div>
             <h1>This is Order Review</h1>
+            <h2>{products.length}</h2>
         </div>
     );
 };
