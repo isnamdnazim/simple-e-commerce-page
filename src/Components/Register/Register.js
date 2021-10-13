62eaec20838223d0b09bdb335681ab5e34538fd3
isnamdnazim <isnamdnazim@gmail.com>
2021-10-14 00:54:19 +0600

    register form added

--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -1,15 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Register.css';
 
 const Register = () => {
     return (
-        <div>
+        <div className="login-form">
             <div>
                 <h2>Register: Create Account</h2>
                 <form onSubmit="">
                     <input type="email" name="" placeholder="Your Email" id="" /><br /><br />
                     <input type="password" name="" placeholder="Password" id="" /><br /><br />
+                    <input type="password" name="" placeholder="Re-Enter Password" id="" /><br /><br />
                     <input type="submit" value="Register" />
                 </form>
+                <p>Already Have a Account! <Link to="/login">Sign In</Link></p>
             </div>
         </div>
     );
