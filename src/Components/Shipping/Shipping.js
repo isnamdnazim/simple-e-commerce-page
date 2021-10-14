55514495dc5ed6c043977f2dcf4867195db61872
isnamdnazim <isnamdnazim@gmail.com>
2021-10-14 20:57:19 +0600

    shipping from added

--- src/Components/Shipping/Shipping.js
+++ src/Components/Shipping/Shipping.js
@@ -1,21 +1,26 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
+import useAuth from '../../Hooks/useAuth';
+import './Shipping.css';
 
 const Shipping = () => {
 
     const { register, handleSubmit, formState: { errors } } = useForm();
-    const onSubmit = data => console.log(data);
+    const { user } = useAuth();
+    const onSubmit = data => {
+        console.log(data);
+    }
 
     return (
         <div>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                {/* register your input into the hook by invoking the "register" function */}
-                <input defaultValue="test" {...register("example")} />
+            <form className="shipping-form" onSubmit={handleSubmit(onSubmit)}>
 
-                {/* include validation with required or other standard HTML validation rules */}
-                <input {...register("exampleRequired", { required: true })} />
-                {/* errors will return when field validation fails  */}
-                {errors.exampleRequired && <span>This field is required</span>}
+                <input placeholder="Name" defaultValue={user.displayName} {...register("name")} />
+                <input placeholder="Email" defaultValue={user.email} {...register("email", { required: true })} />
+                {errors.email && <span className="error">This field is required</span>}
+                <input placeholder="Address" defaultValue="" {...register("address")} />
+                <input placeholder="City" defaultValue="" {...register("city")} />
+                <input placeholder="Phone Number" defaultValue="" {...register("phone")} />
 
                 <input type="submit" />
             </form>
